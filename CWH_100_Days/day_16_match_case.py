21f576f9cf6981eb0b2bc4d727e61b9922d2fa24
Shubham Doshi <spdoshi23@gmail.com>
2026-08-31 00:17:45 +0530

    day 16 in progress

---
+++ CWH_100_Days/day_16_match_case.py
@@ -0,0 +1,18 @@
+# MATCH CASE STATEMENT:
+# To implement switch-case like characteristics very similar to if-else functionality, we use a match case in python
+# 
+# A match statement will compare a given variable’s value to different shapes, also referred to as the pattern.
+#  The main idea is to keep on comparing the variable with all the present patterns until it fits into one.
+# The match case consists of three main entities :
+
+#1. The match keyword
+#2. One or more case clauses
+#3. Expression for each case
+# The case clause consists of a pattern to be matched to the variable, a condition to be evaluated if the pattern matches, and a set of statements to be executed if the pattern matches.
+
+
+
+
+
+
+
