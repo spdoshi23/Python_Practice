a1b47ae9a53c12d3695e70df1d95e92b12e04b31
Shubham Doshi <spdoshi23@gmail.com>
2026-09-10 16:24:40 +0530

    drill 2 complete

---
+++ CWH_100_Days/day_20_drill2.py
@@ -0,0 +1,8 @@
+def introduce(subject, faculty_title):
+    print("Tomorrow we have", subject, "with", faculty_title, ".")
+
+introduce("maths", "mr.singh")
+
+sub_name = "python"
+title = "professor"
+introduce(sub_name, title)
