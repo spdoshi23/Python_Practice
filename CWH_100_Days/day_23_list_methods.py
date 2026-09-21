255201a09359c7dd1cd5dbd478d61a250fdb453d
Shubham Doshi <spdoshi23@gmail.com>
2026-09-21 23:43:34 +0530

    learning list methods

---
+++ CWH_100_Days/day_23_list_methods.py
@@ -0,0 +1,14 @@
+# list.sort()
+# This method sorts the list in ascending order. The original list is updated
+
+# Example 1:
+colors = ["voilet", "indigo", "blue", "green"]
+colors.sort()
+print(colors)                                            # ['blue', 'green', 'indigo', 'voilet']\
+
+num = [4,2,5,3,6,1,2,1,2,8,9,7]
+num.sort()
+print(num)                                               # [1, 1, 2, 2, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+
